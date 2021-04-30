db25bb77d2c44471689b4c6f9f7d09574bd4d5c8
david yoon <gnbwkd@gmail.com>
2021-04-29 22:19:22 -0700

    Top nav scroll transition added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){  
     $('.nav-history').click(function(){
         $('html, body').animate({
-            scrollTop:$('.history-page-wrapper').offset().top -80 
+            scrollTop:$('.history-page-wrapper').offset().top -69 
         }, 300);
     });
     $('.nav-team').click(function(){
         $('html, body').animate({
-            scrollTop:$('.team-page-wrapper').offset().top -80
+            scrollTop:$('.team-page-wrapper').offset().top -69
         },300);
     });
-    if($('#id').offset().top > 80){
-        alert('shit');
-    }
-    
+    $(window).resize(function(){
+        if($(this).width() <= 500){
+            alert('shit');
+        }
+    });
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll >= $('.history-page-wrapper').offset().top -71){
+            $('.nav-wrapper').addClass("nav-scroll");
+        }else{
+            $('.nav-wrapper').removeClass("nav-scroll");
+        }
+    })
 });
