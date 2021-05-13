22e6a6a516a19c2020dd7b66d9909410d24be176
david yoon <gnbwkd@gmail.com>
2021-05-13 15:57:17 -0700

    Carousel added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('mySlides');
+    var dots = document.getElementsByClassName('carousel-dot');
+    if (n > slides.length){
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slide.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " active";
+}
 $(document).ready(function(){  
+
     $('.nav-history').click(function(){
         $('html, body').animate({
             scrollTop:$('.history-page-wrapper').offset().top -69 
@@ -21,5 +48,6 @@ $(document).ready(function(){
         }else{
             $('.nav-wrapper').removeClass("nav-scroll");
         }
-    })
+    });
+    
 });
